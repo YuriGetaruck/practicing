252ea63e698bef1d21d9348207d6e16e50b67c4c
Yuri Constantino Getaruck <90275932+YuriGetaruck@users.noreply.github.com>
2021-09-08 12:29:56 -0300

    trying 2d lists

---
+++ 2D_Lists.py
@@ -0,0 +1,22 @@
+# list of a list
+
+drinks = ["coffe", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks,dinner,dessert]
+
+print(food)
+
+print()
+print()
+
+for i in food:
+    print(i)
+
+
+print()
+print()
+
+print(food[2][0])
+
