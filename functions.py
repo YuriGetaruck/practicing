628dcf8c857e73ecd45865d90519ab5512acc51d
Yuri Constantino Getaruck <90275932+YuriGetaruck@users.noreply.github.com>
2021-09-11 16:35:53 -0300

    functions and it usage

---
+++ functions.py
@@ -0,0 +1,8 @@
+#functions // funcoes
+
+def hello(name, last_name, age):
+    print("Hello there" + name + last_name)
+    print("you are "+str(age)+ " years old")
+
+
+hello(" yuri", " getaruck", 21)
