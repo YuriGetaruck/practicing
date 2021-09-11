a849281c6b252e032ba0459d42099529af249f44
Yuri Constantino Getaruck <90275932+YuriGetaruck@users.noreply.github.com>
2021-09-11 16:27:08 -0300

    index [] useage

---
+++ index.py
@@ -0,0 +1,31 @@
+#index operator []
+
+name = "yuri Getaruck &"
+
+
+
+
+#if name[0].islower :
+#    name = name.capitalize()
+
+first_name = name[:4].upper()
+
+last_name = name[5:].lower()
+
+last_character = name[-1]
+
+print(first_name + " " + last_name)
+
+print(last_character)
+
+
+
+
+
+
+
+
+
+
+
+
